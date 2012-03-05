6321e98b39f27400e704f79d210d5795fe9534de
Safdar Zaman <szaman@ist.tugraz.at>
2012-03-05 10:58:22 +0100

    repair domain updated

--- diagnosis_observers/src/Triggering.py
+++ diagnosis_observers/src/Triggering.py
@@ -6,14 +6,14 @@ from std_msgs.msg import String
 class Triggering_Node(object):
 
     def __init__(self):
-				self.pub = rospy.Publisher('/odometry', String)				
+				self.pub = rospy.Publisher('/Topic1', String)				
         
     def start(self):
-				rospy.init_node('jaguar_node', anonymous=True)
+				rospy.init_node('triggering', anonymous=True)
 				rate = rospy.Rate(10) # 10hz
 				while not rospy.is_shutdown():
-						print "jaguar_node"
-						self.pub.publish(String('jaguar_node_odometry'))
+						print "triggering"
+						self.pub.publish(String('triggering'))
 						rate.sleep()
     
 if __name__ == '__main__':
